ade1b5ec748d0d670e3c017e3de984b4fe61bc52
ankurshahi80 <shahiankur80@gmail.com>
2022-03-08 21:54:20 -0500

    object-class

---
+++ demos/object-class/src/com/revature/model/Person.java
@@ -0,0 +1,69 @@
+package com.revature.model;
+
+import java.util.Objects;
+
+// JavaBean (with the exception of not implementing Serializable, which is old)
+public class Person {
+
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    //No-arg constructor
+    public Person() {
+
+    }
+
+    // Parameterized constructor
+    public Person(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return age == person.age && Objects.equals(firstName, person.firstName) && Objects.equals(lastName, person.lastName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, lastName, age);
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
